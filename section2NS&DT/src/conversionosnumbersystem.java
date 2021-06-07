6b545fdad0d53631bf6490aa0d0dc93652380310
sakshigupta1102 <sakshisg2000@gmail.com>
2021-06-07 19:09:48 +0530

    third commit

---
+++ section2NS&DT/src/conversionosnumbersystem.java
@@ -0,0 +1,13 @@
+
+public class conversionosnumbersystem {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//           conversion from one number system to another
+//           convert binary no. in decimal
+//           decimal in binary
+//           octal to decimal
+//           decimal to octal 
+	}
+
+}
